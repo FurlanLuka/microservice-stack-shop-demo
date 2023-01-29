9ac25271374be626907ee7cc32f242c9438f3ad5
Luka Furlan <100034176+FurlanLuka@users.noreply.github.com>
2023-01-29 20:36:26 +0100

    Added terraform config

---
+++ infrastructure/cloud/variables.tf
@@ -0,0 +1,19 @@
+variable "environment_name" {
+  default = "demo"
+}
+
+variable "domain" {
+  default = "cloud.microservice-stack.com"
+}
+
+variable "zone_id" {
+  default = "Z0524984X82EET1H7DYL"
+}
+
+variable "output_secrets" {
+  default = true
+}
+
+variable "load_balancer_url" {
+  default = ""
+}
